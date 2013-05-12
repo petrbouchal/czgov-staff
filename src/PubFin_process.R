6faa3ab90342933e6256e38c8de0f24648e249ed
Petr Bouchal <pbouchal@gmail.com>
2013-05-12 01:19:09 +0100

    First commit

--- src/PubFin_process.R
+++ src/PubFin_process.R
@@ -1,5 +1,5 @@
-isdata <- read.csv('../data-input/VydajeVysledovka_20130511_2309.csv')
-bsdata <- read.csv('../data-input/VydajeDruhove_20130511_2309.csv')
+inc <- read.csv('../data-input/VydajeVysledovka_20130511_2309.csv')
+bud <- read.csv('../data-input/VydajeDruhove_20130511_2309.csv')
 
-bsdata$podil <- bsdata$pozmenach/bsdata$skutecnost
-hist(bsdata$podil[bsdata$orgyear==2012],breaks=1000000000,xlim=c(0,2))
+bud$podil <- bud$pozmenach/bud$skutecnost
+hist(bud$podil[bud$orgyear==2012],breaks=1000,xlim=c(0.1,1.9))
