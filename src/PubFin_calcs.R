8b6c27d420db6ff57ca2963e1af4312f398628bb
Petr Bouchal <pbouchal@gmail.com>
2013-05-12 22:25:51 +0100

    Clean up, small fixes, and documentation

--- src/PubFin_calcs.R
+++ src/PubFin_calcs.R
@@ -16,6 +16,7 @@ rowsum_ch$pcoverspend <- rowsum_ch$overspend/rowsum_ch$pozmenach
 #   scale_y_continuous(limits=c(-10,10))
 # plot
 
+# calculate indices from year to year and ratios between plan and actual
 budw$revisionpodil2010 = budw$pozmenach2010/budw$rozpocet2010
 budw$realitypodil2010 = budw$skutecnost2010/budw$rozpocet2010
 budw$realityindex2010 = 1
@@ -31,6 +32,7 @@ budw$revisionpodil2012 = budw$pozmenach2012/budw$rozpocet2012
 budw$realitypodil2012 = budw$skutecnost2012/budw$rozpocet2012
 budw$realityindex2012 = budw$skutecnost2012/budw$skutecnost2011
 
+# indexc means 'cumulative': total change from 2010
 budw$realityindexc2010 = 1
 budw$budgetindexc2010 = 1
 budw$budgetindexc2011 = budw$rozpocet2011/budw$rozpocet2010
@@ -38,39 +40,51 @@ budw$budgetindexc2012 = budw$rozpocet2012/budw$rozpocet2010
 budw$realityindexc2012 = budw$skutecnost2012/budw$skutecnost2010
 budw$realityindexc2011 = budw$skutecnost2011/budw$skutecnost2010
 
+# create long file from wide
 budl <- melt(budw)
 
+# subset for pay data only, for both chapters and orgs
+# then subset for index variables only and create appropriate year variable
 budl_platy_OSS <- subset(budl,budl$rowname=="Platy zaměstnanců v pracovním poměru" & budw$orgtype=="OSS")
 budl_platyindex_OSS <- subset(budl_platy_OSS,grepl('indexc', budl_platy_OSS$variable))
 budl_platyindex_OSS$year <- str_sub(budl_platyindex_OSS$variable,-4,-1)
 budl_platyindex_OSS$variable <- sub('[01234]{4}', '', budl_platyindex_OSS$variable,)
 budl_platyindex_OSS$group <- paste(budl_platyindex_OSS$variable,
                                    budl_platyindex_OSS$orgid, sep='_')
-
+# same as above for chapters
 budl_platy_ch <- subset(budl,budl$rowname=="Platy zaměstnanců v pracovním poměru" & budw$orgtype=="chapter")
 budl_platyindex_ch <- subset(budl_platy_ch,grepl('indexc', budl_platy_ch$variable))
 budl_platyindex_ch$year <- str_sub(budl_platyindex_ch$variable,-4,-1)
 budl_platyindex_ch$variable <- sub('[01234]{4}', '', budl_platyindex_ch$variable,)
 
+# create group variable to allow ggplot to group points correctly for drawing lines
+# this plot shows one line for each org's plan and one line for each org's outturn,
+# differentiated by colour and grouped by chapter
 budl_platyindex_ch$group <- paste(budl_platyindex_ch$variable,
                                    budl_platyindex_ch$orgid, sep='_')
+# plot this. Note ggplot drops whole line if even one point is outside scale limits.
 plot <- ggplot(data=budl_platyindex_OSS, aes(x=year, y=value,
                                              group=group, colour=variable))+
   geom_line(alpha=1) +
   facet_wrap(~ orgchaptername, scales='free')
 plot
 
+# same plot but for chapters only
 plot <- ggplot(data=budl_platyindex_ch, aes(x=year, y=value,
                                              group=group, colour=variable))+
   geom_line(alpha=1) +
   facet_wrap(~ orgchaptername, scales='free')
 plot
 
+# for chapter only - subset pay data from original long file
 bud_platy_ch <- subset(bud,bud$rowname=="Platy zaměstnanců v pracovním poměru" & bud$orgtype=="chapter")
+# calculate absolute and percentage over/underspend
 bud_platy_ch$overspend <- bud_platy_ch$skutecnost-bud_platy_ch$rozpocet
 bud_platy_ch$pcoverspend <- bud_platy_ch$overspend/bud_platy_ch$rozpocet
+# reshape into long to allow charting
 budl_platy_ch <- melt(bud_platy_ch)
 
+# chart: shows overspends per chapter by year. Can select absolute or % in subset
 plot <- ggplot(subset(budl_platy_ch, variable=='pcoverspend'),
                aes(x=orgyear, y=value, fill=orgyear,
                    group=orgchaptername))+
@@ -78,10 +92,9 @@ plot <- ggplot(subset(budl_platy_ch, variable=='pcoverspend'),
   facet_wrap(~ orgchaptername)
 plot
 
+# chart total overspend per year - not working properly, 
+# will need to summarise manually and then chart
 plot <- ggplot(subset(budl_platy_ch, variable=='overspend'),
                aes(x=orgyear, y=value, fill=orgyear, group=1))+
   geom_bar(stat='sum',position='dodge')
 plot
-
-hist(budw_platy_OSS$budgetindex1012,breaks=400,xlim=c(0.5,1.5))
-plot(budw_platy_OSS$rozpocet2010,budw_platy_OSS$budgetindex1012,ylim=c(0.8,1.2),pch=19)
