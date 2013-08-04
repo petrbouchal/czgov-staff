7ca5a0d6cb8e8fbb4019388bc8d5b1a2d4fb9c89
Petr Bouchal <pbouchal@gmail.com>
2013-08-04 18:37:16 +0100

    code structure cleanup

--- src/lib/SavePlot.R
+++ src/lib/SavePlot.R
@@ -1,10 +1,12 @@
 SavePlot <- function (plotname='Plot', plotformat='eps', ffamily='Helvetica',
                       splot=last_plot() ,ploth=210/2, plotw=14) {
   try(dev.off(),silent=TRUE)
-  plotobjdir <- './charts/ACSES chart objects/'
-  plotimagedir <- './charts/ACSES charts/'
+  plotobjdir <- './charts-output/charts-objects/'
+  plotimagedir <- './charts-output/charts-images/'
+  plotdatadir <- './charts-output/charts-data//'
   plotimagepath = paste0(plotimagedir,plotname,'.',plotformat)
   plotobjpath = paste0(plotobjdir,plotname,'.','ggp')
+  plotdatapath = paste0(plotdatadir,plotname,'.','ggp')
   if(plotformat=='pdf') {
     ggsave(plotimagepath, plot=splot, family=ffamily, device=cairo_pdf,
            height=ph, width=pw, units='cm')  
@@ -16,6 +18,6 @@ SavePlot <- function (plotname='Plot', plotformat='eps', ffamily='Helvetica',
            height=ploth, width=plotw, units='cm')
   }
   save(splot,file=plotobjpath)
-  write.csv(splot$data,file=paste0(plotimagedir,plotname,'_data.csv'))
+  write.csv(splot$data,file=paste0(plotdatadir,plotname,'_data.csv'))
   splot
 }
